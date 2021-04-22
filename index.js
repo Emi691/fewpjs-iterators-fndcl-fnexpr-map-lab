133b9667a7d3fc918a21aac68d3eeaedb025a9c8
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-04-22 13:44:46 -0700

    commit tree 17a0d293142cb3dd33989468028ef5361ebedd7a

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased(){
+  return tutorials.map(tutorial => capitalize(tutorial))
+}
+
+
+function capitalize(tutorial){
+  let newTutorial = tutorial.split(' ').map(function(word) { 
+    return word.charAt(0).toUpperCase() + word.slice(1)
+  }).join(' ')
+
+  return newTutorial
 }
